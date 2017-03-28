51e59fa0377c5c14281e2470cb906dbaf79db936
chetantarale <chetan_tarale@yahoo.com>
2017-03-28 15:54:40 +0530

    adds -nv flag to wget command

--- version/1_6_0.sh
+++ version/1_6_0.sh
@@ -2,10 +2,10 @@
 
 # Install Clojure 1.6.0
 echo "================= Installing Clojure-1.6.0 ==================="
-sudo wget http://repo1.maven.org/maven2/org/clojure/clojure/1.6.0/clojure-1.6.0.zip
-sudo unzip clojure-1.6.0.zip
+wget -nv http://repo1.maven.org/maven2/org/clojure/clojure/1.6.0/clojure-1.6.0.zip
+unzip clojure-1.6.0.zip
 mkdir $HOME/lib && cp clojure-1.6.0/clojure-1.6.0.jar $HOME/lib
-sudo rm -rf clojure-1.6.0*
+rm -rf clojure-1.6.0*
 cat >/usr/local/bin/clj <<EOF
 #!/bin/bash
 if [[ $# -eq 0 ]]; then
@@ -14,4 +14,4 @@ else
   java -server -cp $HOME/lib/clojure-1.6.0.jar clojure.main $1 -- "$@"
 fi
 EOF
-sudo chmod a+x /usr/local/bin/clj
+chmod a+x /usr/local/bin/clj
